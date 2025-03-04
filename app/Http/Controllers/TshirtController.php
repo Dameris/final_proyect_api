50ef1c0226447d94aeba9f63b83323817594171e
Daniel Merino <danimerinoo02@gmail.com>
2025-03-04 11:09:50 +0100

    - feat: first commit, create API proyect

---
+++ app/Http/Controllers/TshirtController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tshirt;
+use App\Http\Requests\StoreTshirtRequest;
+use App\Http\Requests\UpdateTshirtRequest;
+
+class TshirtController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTshirtRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tshirt $tshirt)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tshirt $tshirt)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTshirtRequest $request, Tshirt $tshirt)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tshirt $tshirt)
+    {
+        //
+    }
+}
