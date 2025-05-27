76438eaa0df917837fe2653d52c399ef7d824d95
Daniel Merino <danimerinoo02@gmail.com>
2025-05-28 01:14:15 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -67,5 +67,3 @@ Route::middleware(['auth', 'verified',])->group(function () {
 });
 
 Route::get('/tshirts/{tshirt}', [TshirtController::class, 'show'])->name('tshirts.show');
-
-Route::get('/tshirts/{tshirt}', [TshirtController::class, 'show'])->name('tshirts.show');
