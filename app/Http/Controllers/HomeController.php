61aa63315296225cf73c0a8230bcf2f2c771ded0
Daniel Merino <danimerinoo02@gmail.com>
2026-04-29 12:59:41 +0200

    - fix: changed how "HomeController" takes the events to only show the current or future events.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Inertia\Inertia;
 use App\Models\Tshirt;
 use App\Models\Event;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -12,7 +13,14 @@ class HomeController extends Controller
     {
         return Inertia::render('Home', [
             'tshirts' => Tshirt::orderBy("id", "asc")->limit(1)->get(),
-            'events' => Event::orderBy('start_date')->take(3)->get(),
+            'events' => Event::where('end_date', '>=', Carbon::now())
+                ->orWhere(function ($query) {
+                    $query->whereNull('end_date')
+                        ->where('start_date', '>=', Carbon::now());
+                })
+                ->orderBy('start_date')
+                ->take(3)
+                ->get(),
         ]);
     }
 }
